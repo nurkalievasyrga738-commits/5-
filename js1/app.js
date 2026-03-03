a5f0784ac2790535dd25b767ec3cfddcc99dbfed
syrga <nurkalievasyrga738@gmail.com>
2026-03-03 12:31:08 +0600

    first coment

---
+++ js1/app.js
@@ -0,0 +1,57 @@
+// let number = 20
+// let number2 = 40
+
+// console.log(number < number2)
+// = - ичине салып берет
+
+// let = ach = "ыы"
+// let  nul 
+// let  bigInt = BigInt(123)
+// let  cookie = false;
+// let  user = null
+// let sym = Symbol("Id") 
+
+
+// console.log(typeof number) 
+// console.log(typeof ach)
+// console.log(typeof nul )
+// console.log(typeof bi)
+// console.log(typeof cookie)
+// console.log(typeof user)
+// console.log(typeof sym)
+
+const newName = prompt("Атынды жаз")
+
+console.log(newName)
+
+const newlastName = prompt("Фамилия")
+
+console.log(newlastName)
+
+const newMovi = prompt("Любимый мульт")
+
+console.log(newMovi)
+
+const newInfo = prompt("Ысык-Колду кайсы тоолор курчайт")
+
+console.log(newInfo)
+
+const newInfo2 = prompt("Чуй облустун эн бийик чокусу жана чокунун саны")
+
+console.log(newInfo2)
+
+// типы данных 8
+// приметивные типы,ссылочные
+// 7 примет, 1 ссылочные
+
+// 
+// string - "",
+// number - 1,2,3,4,5,6,7..
+// Boolean - false, true
+// null-нечего нету
+// undefined - не могу найти
+// Symbol - уникальное значение
+// BigInt - 1000000000000 - 100a
+// object - {"ываыаы"}
+
+
---
+++ js2/app.js
@@ -0,0 +1,41 @@
+
+// типы данных 8
+// приметивные типы,ссылочные
+// 7 примет, 1 ссылочные
+
+// 
+// string - "",
+// number - 1,2,3,4,5,6,7..
+// Boolean - false, true
+// null-нечего нету
+// undefined - не могу найти
+// Symbol - уникальное значение
+// BigInt - 1000000000000 - 100a
+// object - {"ываыаы"}
+
+// const age = 20
+// const firstName = prompt("aт жаз")
+
+// console.log(typeof age) - типы
+
+// console.log(firstName + age)
+// console.log("Сенин атын" + firstName"сен"+ age)
+
+
+
+
+// const num = +prompt("сан жаз")
+// const num2 = +prompt("дагы сан жаз")
+
+// console.log("жообу:")
+
+// console.log("алуу",num-num2)
+// console.log("болуу",num/num2)
+// console.log("кобойтуу",num*num2)
+// console.log("кошуу",num+num2)
+
+// let number = 20
+// let number2 = "40"
+
+// console.log(number > number2)
+
